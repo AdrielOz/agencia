4f48eb330064d91387dc872f0818e71034e0470c
Sergio Gutiérrez <47606076+AdrielOz@users.noreply.github.com>
2021-08-27 11:23:59 -0600

    1
    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Index2Component } from './index2/index2.component';
+import { Index3Component } from './index3/index3.component';
+import { Index4Component } from './index4/index4.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Index2Component,
+    Index3Component,
+    Index4Component,
+
   ],
   imports: [
     BrowserModule,
